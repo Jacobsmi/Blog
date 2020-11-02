b2dcece95d3cb6c86b38880a2dfcd3e211993c96
jacobsm84 <jacobsm84@gmail.com>
2020-11-02 11:09:41 -0500

    Create BlogPost's dynamically

--- src/Blog.js
+++ src/Blog.js
@@ -1,13 +1,32 @@
+import React from "react";
 import BlogHeader from './components/BlogHeader';
 import BlogPost from './components/BlogPost'
 
-function Blog() {
-    return (
-        <div className="Blog">
-            <BlogHeader />
-            <BlogPost />
-            <BlogPost />
-        </div>
-    );
+export default class Blog extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            posts: [],
+
+        }
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/allposts')
+        .then(resp => resp.json())
+        .then(data=> this.setState({posts: data}))
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+    render(){
+        console.log("POSTS STATE", this.state.posts)
+        return (
+            <div className="Blog">
+                <BlogHeader />
+                {this.state.posts.map(post=>{
+                    return(<BlogPost key={post.id} title={post.title} />)
+                })}
+            </div>
+        );
+    }
 }
-export default Blog;
--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -1,11 +1,14 @@
-import './styles/BlogHeader.css'
+import React from "react";
+import './styles/BlogHeader.css';
 
-const BlogHeader = () =>{
-    return (
-        <div className="BlogHeader">
-            <h1>Michael Jacobs' Blog</h1>
-        </div>
-    );
+class BlogHeader extends React.Component{
+    render() {
+        return (
+            <div className="BlogHeader">
+                <h1>My Blog</h1>
+            </div>
+        );
+    }
 }
 
 export default BlogHeader;
